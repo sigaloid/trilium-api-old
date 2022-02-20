56724449cc7b13c7ff07def21c640f99cb3ffb35
Matt <69441971+sigaloid@users.noreply.github.com>
2022-02-20 17:45:48 -0500

    some changes

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use nanoserde::DeJsonErr;
 mod create_note;
 mod get_note;
 mod patch_note;
-mod search;
+pub mod search;
 
 pub struct Trilium {
     url: String,
@@ -60,7 +60,79 @@ impl Trilium {
         })
     }
 
-    pub fn create_note(trilium: &Trilium, note: Note) -> Result<NoteResponse, crate::Error> {
+    pub fn search_notes(
+        trilium: &Trilium,
+        search_options: SearchOptions,
+    ) -> Result<SearchResults, crate::Error> {
+        let mut querystr = String::new();
+        querystr.push_str(&format!("search=\"{}\"", search_options.search));
+        if search_options.fast_search {
+            querystr.push_str("&fastSearch=true");
+        }
+        if search_options.include_archived_notes {
+            querystr.push_str("&includeArchivedNotes=true");
+        }
+        if let Some(ancestor_id) = search_options.ancestor_note_id {
+            querystr.push_str(&format!("&ancestorNoteId={}", ancestor_id));
+        }
+        if let Some(ancestor_depth) = search_options.ancestor_depth {
+            querystr.push_str(&format!(
+                "&ancestorDepth={}",
+                match ancestor_depth {
+                    search::DepthOptions::LessThan(a) => format!("lt{}", a),
+                    search::DepthOptions::Exactly(a) => format!("eq{}", a),
+                    search::DepthOptions::GreaterThan(a) => format!("gt{}", a),
+                }
+            ));
+        }
+        if let Some(order_by) = search_options.order_by {
+            querystr.push_str(&format!("&orderBy={}", order_by));
+        }
+        if let Some(order_direction) = search_options.order_direction {
+            querystr.push_str(&format!(
+                "&orderDirection={}",
+                match order_direction {
+                    search::OrderDirection::Ascending => "asc",
+                    search::OrderDirection::Descending => "dec",
+                }
+            ));
+        }
+        if let Some(limit) = search_options.limit {
+            querystr.push_str(&format!("&limit={}", limit));
+        }
+        if search_options.debug {
+            querystr.push_str("&debug=true");
+        }
+
+        let req = trilium
+            .agent
+            .get(&format!("{}/etapi/notes?{}", trilium.url, querystr))
+            .call();
+        match req {
+            Ok(response) => {
+                if let Ok(string) = response.into_string() {
+                    let parse: Result<SearchResults, DeJsonErr> =
+                        nanoserde::DeJson::deserialize_json(&string);
+                    if let Ok(note_response) = parse {
+                        Ok(note_response)
+                    } else {
+                        Err(Error::InvalidServerResponse(Some(string)))
+                    }
+                } else {
+                    Err(Error::InvalidServerResponse(None))
+                }
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                match e {
+                    ureq::Error::Status(_, _) => Err(Error::WrongPassword),
+                    ureq::Error::Transport(_) => Err(Error::InvalidUrl),
+                }
+            }
+        }
+    }
+
+    pub fn create_note(trilium: &Self, note: Note) -> Result<NoteResponse, crate::Error> {
         let req = trilium
             .agent
             .post(&format!("{}/etapi/create-note", trilium.url))
@@ -86,7 +158,7 @@ impl Trilium {
         }
     }
 
-    pub fn delete_note(trilium: &Trilium, id: impl ToString) -> Result<(), crate::Error> {
+    pub fn delete_note(trilium: &Self, id: impl ToString) -> Result<(), crate::Error> {
         let req = trilium
             .agent
             .delete(&format!("{}/etapi/notes/{}", trilium.url, id.to_string()))
@@ -107,7 +179,7 @@ impl Trilium {
     }
 
     pub fn get_note(
-        trilium: &Trilium,
+        trilium: &Self,
         id: impl ToString,
     ) -> Result<get_note::NoteResult, crate::Error> {
         let req = trilium
@@ -135,7 +207,7 @@ impl Trilium {
         }
     }
     pub fn patch_note(
-        trilium: &Trilium,
+        trilium: &Self,
         note: &patch_note::NoteResult,
     ) -> Result<patch_note::NoteResult, crate::Error> {
         let req = trilium
@@ -172,6 +244,8 @@ pub enum Error {
 
 use nanoserde::DeJson;
 use nanoserde::SerJson;
+use search::SearchOptions;
+use search::SearchResults;
 use ureq::MiddlewareNext;
 use ureq::Request;
 
--- src/search.rs
+++ src/search.rs
@@ -3,24 +3,24 @@ use nanoserde::{DeJson, DeJsonErr, SerJson};
 
 #[derive(SerJson, DeJson)]
 pub struct SearchOptions {
-    search: String,
+    pub search: String,
     #[nserde(rename = "fastSearch")]
-    fast_search: bool, // Ignore note content
+    pub fast_search: bool, // Ignore note content
     #[nserde(rename = "includeArchivedNotes")]
-    include_archived_notes: bool, // Include archived notes
+    pub include_archived_notes: bool, // Include archived notes
     #[nserde(rename = "ancestorNoteId")]
-    ancestor_note_id: Option<String>, // Search only in subtree (id)
+    pub ancestor_note_id: Option<String>, // Search only in subtree (id)
     #[nserde(rename = "ancestorDepth")]
-    ancestor_depth: Option<DepthOptions>, // Options for depth search
+    pub ancestor_depth: Option<DepthOptions>, // Options for depth search
     #[nserde(rename = "orderBy")]
-    order_by: Option<String>, // Examples include "title", "#publicationDate", "isProtected", "isArchived", "dateCreated", "dateModified", "utcDateCreated", "utcDateModified", "parentCount", "childrenCount", "attributeCount", "labelCount", "ownedLabelCount", "relationCount", "ownedRelationCount", "relationCountIncludingLinks", "ownedRelationCountIncludingLinks", "targetRelationCount", "targetRelationCountIncludingLinks", "contentSize", "noteSize", "revisionCount"
+    pub order_by: Option<String>, // Examples include "title", "#publicationDate", "isProtected", "isArchived", "dateCreated", "dateModified", "utcDateCreated", "utcDateModified", "parentCount", "childrenCount", "attributeCount", "labelCount", "ownedLabelCount", "relationCount", "ownedRelationCount", "relationCountIncludingLinks", "ownedRelationCountIncludingLinks", "targetRelationCount", "targetRelationCountIncludingLinks", "contentSize", "noteSize", "revisionCount"
     #[nserde(rename = "orderDirection")]
-    order_direction: Option<OrderDirection>, // Direction of ordering
-    limit: Option<usize>, // Search limit
-    debug: bool,          // Enable debugging in response
+    pub order_direction: Option<OrderDirection>, // Direction of ordering
+    pub limit: Option<usize>, // Search limit
+    pub debug: bool,          // Enable debugging in response
 }
 impl SearchOptions {
-    fn default(search_query: impl ToString) -> Self {
+    pub fn default(search_query: impl ToString) -> Self {
         Self {
             search: search_query.to_string(),
             fast_search: false,
@@ -35,55 +35,26 @@ impl SearchOptions {
     }
 }
 #[derive(SerJson, DeJson)]
-enum OrderDirection {
+pub enum OrderDirection {
     #[nserde(rename = "asc")]
     Ascending,
     #[nserde(rename = "dec")]
     Descending,
 }
 #[derive(SerJson, DeJson)]
-enum DepthOptions {
+pub enum DepthOptions {
     LessThan(usize),
     Exactly(usize),
     GreaterThan(usize),
 }
 
-pub fn search_notes(
-    trilium: &Trilium,
-    search_options: SearchOptions,
-) -> Result<SearchResults, crate::Error> {
-    let req = trilium
-        .agent
-        .post(&format!("{}/etapi/notes", trilium.url))
-        .send_string(&search_options.serialize_json());
-    match req {
-        Ok(response) => {
-            if let Ok(string) = response.into_string() {
-                let parse: Result<SearchResults, DeJsonErr> =
-                    nanoserde::DeJson::deserialize_json(&string);
-                if let Ok(note_response) = parse {
-                    Ok(note_response)
-                } else {
-                    Err(Error::InvalidServerResponse(Some(string)))
-                }
-            } else {
-                Err(Error::InvalidServerResponse(None))
-            }
-        }
-        Err(e) => match e {
-            ureq::Error::Status(_, _) => Err(Error::WrongPassword),
-            ureq::Error::Transport(_) => Err(Error::InvalidUrl),
-        },
-    }
-}
-
 #[derive(SerJson, DeJson)]
 pub struct SearchResults {
     #[nserde(rename = "results")]
-    results: Vec<SearchResult>,
+    pub results: Vec<SearchResult>,
 
     #[nserde(rename = "debugInfo")]
-    debug_info: DebugInfo,
+    pub debug_info: DebugInfo,
 }
 
 #[derive(SerJson, DeJson)]
@@ -92,71 +63,71 @@ pub struct DebugInfo {}
 #[derive(SerJson, DeJson)]
 pub struct SearchResult {
     #[nserde(rename = "noteId")]
-    note_id: String,
+    pub note_id: String,
 
     #[nserde(rename = "title")]
-    title: String,
+    pub title: String,
 
     #[nserde(rename = "type")]
-    result_type: String,
+    pub result_type: String,
 
     #[nserde(rename = "mime")]
-    mime: String,
+    pub mime: String,
 
     #[nserde(rename = "isProtected")]
-    is_protected: bool,
+    pub is_protected: bool,
 
     #[nserde(rename = "attributes")]
-    attributes: Vec<Attribute>,
+    pub attributes: Vec<Attribute>,
 
     #[nserde(rename = "parentNoteIds")]
-    parent_note_ids: Vec<String>,
+    pub parent_note_ids: Vec<String>,
 
     #[nserde(rename = "childNoteIds")]
-    child_note_ids: Vec<String>,
+    pub child_note_ids: Vec<String>,
 
     #[nserde(rename = "parentBranchIds")]
-    parent_branch_ids: Vec<String>,
+    pub parent_branch_ids: Vec<String>,
 
     #[nserde(rename = "childBranchIds")]
-    child_branch_ids: Vec<String>,
+    pub child_branch_ids: Vec<String>,
 
     #[nserde(rename = "dateCreated")]
-    date_created: String,
+    pub date_created: String,
 
     #[nserde(rename = "dateModified")]
-    date_modified: String,
+    pub date_modified: String,
 
     #[nserde(rename = "utcDateCreated")]
-    utc_date_created: DebugInfo,
+    pub utc_date_created: DebugInfo,
 
     #[nserde(rename = "utcDateModified")]
-    utc_date_modified: DebugInfo,
+    pub utc_date_modified: DebugInfo,
 }
 
 #[derive(SerJson, DeJson)]
 pub struct Attribute {
     #[nserde(rename = "attributeId")]
-    attribute_id: String,
+    pub attribute_id: String,
 
     #[nserde(rename = "noteId")]
-    note_id: String,
+    pub note_id: String,
 
     #[nserde(rename = "type")]
-    attribute_type: String,
+    pub attribute_type: String,
 
     #[nserde(rename = "name")]
-    name: String,
+    pub name: String,
 
     #[nserde(rename = "value")]
-    value: String,
+    pub value: String,
 
     #[nserde(rename = "position")]
-    position: i64,
+    pub position: i64,
 
     #[nserde(rename = "isInheritable")]
-    is_inheritable: bool,
+    pub is_inheritable: bool,
 
     #[nserde(rename = "utcDateModified")]
-    utc_date_modified: DebugInfo,
+    pub utc_date_modified: DebugInfo,
 }
